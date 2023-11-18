3ba03f6f759a4e4543c28acfeb23a97c5a221d4b
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-11-17 20:07:48 -0800

    Add label to links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,13 +11,24 @@ import clsx from "clsx";
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: "Home", href: "/dashboard", icon: HomeIcon },
+  {
+    name: "Home",
+    href: "/dashboard",
+    icon: HomeIcon,
+    "aria-label": "Home Page Link",
+  },
   {
     name: "Invoices",
     href: "/dashboard/invoices",
     icon: DocumentDuplicateIcon,
+    "aria-label": "Invoices Page Link",
   },
-  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+  {
+    name: "Customers",
+    href: "/dashboard/customers",
+    icon: UserGroupIcon,
+    "aria-label": "Customers Page Link",
+  },
 ];
 
 export default function NavLinks() {
